2226a9a0e53cdc3e90bcc201f311a35cc3d06a2d
Matthew Suozzo <matthew.suozzo@gmail.com>
2014-12-26 18:14:37 -0500

    added galois field multiplication code

---
+++ src/make_gf_table.c
@@ -0,0 +1,22 @@
+#include "aes.h"
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+  int i = 0;
+  int j = 0;
+  char m[6] = {2, 3, 9, 0xb, 0xd, 0xe};
+  for (j=0; j<6; j++) {
+    printf("  {");
+    for (i=1; i<0xfe; i++) {
+      printf("0x%x, ", gf_mult(m[j], i));
+    }
+    printf("0x%x", gf_mult(m[j], 0xff));
+    if (j == 5) {
+      printf("}\n");
+    } else {
+      printf("},\n");
+    }
+  }
+}
+
