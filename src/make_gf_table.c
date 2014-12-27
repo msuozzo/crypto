d15ea7b864b983fbf2f8a4e7309d5d63bc238e8a
Matthew Suozzo <matthew.suozzo@gmail.com>
2014-12-26 20:39:35 -0500

    fixed mult table - missing 0xff

--- src/make_gf_table.c
+++ src/make_gf_table.c
@@ -8,10 +8,10 @@ int main(int argc, char **argv) {
   char m[6] = {2, 3, 9, 0xb, 0xd, 0xe};
   for (j=0; j<6; j++) {
     printf("  {");
-    for (i=1; i<0xfe; i++) {
-      printf("0x%x, ", gf_mult(m[j], i));
+    for (i=1; i<=0xfe; i++) {
+      printf("0x%x, ", gf_mult_calc(m[j], i));
     }
-    printf("0x%x", gf_mult(m[j], 0xff));
+    printf("0x%x", gf_mult_calc(m[j], 0xff));
     if (j == 5) {
       printf("}\n");
     } else {
