c25ef3f712e1db3da566cc3474881d09505fa0f1
Matthew Suozzo <matthew.suozzo@gmail.com>
2014-12-26 20:40:34 -0500

    passes first spec 128 example

--- src/aes.c
+++ src/aes.c
@@ -20,6 +20,7 @@ byte *expand_key(byte *key, byte *expansion, int Nk) {
   for (i = Nk; i < Nb * (Nr+1); i++) {
     temp_word = expansion + 4*(i-1);
     if (i % Nk == 0) {
+      // approximation of multiplication mod 0x011b
       rcon = i/Nk > 8 ? 0x1b<<(i/Nk - 9) : 1<<(i/Nk - 1);
       expansion[4*i] = expansion[4*(i-Nk)] ^ sub_bytes(temp_word[1]) ^ rcon;
       expansion[4*i+1] = expansion[4*(i-Nk)+1] ^ sub_bytes(temp_word[2]);
@@ -48,6 +49,76 @@ byte *expand_key(byte *key, byte *expansion, int Nk) {
 }
 
 
+void do_encrypt(byte *sbox, byte *key, int Nr) {
+  int r, i, j;
+  byte *tmp, *s, *sp;
+  // temporary sboxes
+  byte tmp_sbox1[16] = {0};
+  byte tmp_sbox2[16] = {0};
+  s = tmp_sbox1;
+  sp = tmp_sbox2;
+  // AddRoundKey
+  for (i = 0; i < 16; i++)
+    s[i] = sbox[i] ^ key[i];
+//  for (i = 0; i < 16; i++) {
+//    printf("%2x", s[i]);
+//    if ((i-3) % 4 == 0)
+//      printf("\n");
+//  }
+//  printf("\n");
+  for (r = 1; r < Nr; r++) {
+    // SubBytes
+    for (i = 0; i < 16; i++)
+      s[i] = sub_bytes(s[i]);
+//    for (i = 0; i < 16; i++) {
+//      printf("%2x", s[i]);
+//      if ((i-3) % 4 == 0)
+//        printf("\n");
+//    }
+//    printf("\n");
+    // ShiftRows, MixColumns, and AddRoundKey
+    sp[0] = gf_mult(0x2, s[0]) ^ gf_mult(0x3, s[5]) ^ s[10] ^ s[15] ^ key[16*r];
+    sp[1] = s[0] ^ gf_mult(0x2, s[5]) ^ gf_mult(0x3, s[10]) ^ s[15] ^ key[16*r + 1];
+    sp[2] = s[0] ^ s[5] ^ gf_mult(0x2, s[10]) ^ gf_mult(0x3, s[15]) ^ key[16*r + 2];
+    sp[3] = gf_mult(0x3, s[0]) ^ s[5] ^ s[10] ^ gf_mult(0x2, s[15]) ^ key[16*r + 3];
+    sp[4] = gf_mult(0x2, s[4]) ^ gf_mult(0x3, s[9]) ^ s[14] ^ s[3] ^ key[16*r + 4];
+    sp[5] = s[4] ^ gf_mult(0x2, s[9]) ^ gf_mult(0x3, s[14]) ^ s[3] ^ key[16*r + 5];
+    sp[6] = s[4] ^ s[9] ^ gf_mult(0x2, s[14]) ^ gf_mult(0x3, s[3]) ^ key[16*r + 6];
+    sp[7] = gf_mult(0x3, s[4]) ^ s[9] ^ s[14] ^ gf_mult(0x2, s[3]) ^ key[16*r + 7];
+    sp[8] = gf_mult(0x2, s[8]) ^ gf_mult(0x3, s[13]) ^ s[2] ^ s[7] ^ key[16*r + 8];
+    sp[9] = s[8] ^ gf_mult(0x2, s[13]) ^ gf_mult(0x3, s[2]) ^ s[7] ^ key[16*r + 9];
+    sp[10] = s[8] ^ s[13] ^ gf_mult(0x2, s[2]) ^ gf_mult(0x3, s[7]) ^ key[16*r + 10]; //4f 03 (19)
+    sp[11] = gf_mult(0x3, s[8]) ^ s[13] ^ s[2] ^ gf_mult(0x2, s[7]) ^ key[16*r + 11]; //b2 80 (65)
+    sp[12] = gf_mult(0x2, s[12]) ^ gf_mult(0x3, s[1]) ^ s[6] ^ s[11] ^ key[16*r + 12];
+    sp[13] = s[12] ^ gf_mult(0x2, s[1]) ^ gf_mult(0x3, s[6]) ^ s[11] ^ key[16*r + 13];
+    sp[14] = s[12] ^ s[1] ^ gf_mult(0x2, s[6]) ^ gf_mult(0x3, s[11]) ^ key[16*r + 14];
+    sp[15] = gf_mult(0x3, s[12]) ^ s[1] ^ s[6] ^ gf_mult(0x2, s[11]) ^ key[16*r + 15];
+    tmp = s;
+    s = sp;
+    sp = tmp;
+//    for (i = 0; i < 16; i++) {
+//      printf("%2x", s[i]);
+//      if ((i-3) % 4 == 0)
+//        printf("\n");
+//    }
+//    printf("\n");
+  }
+  // SubBytes, ShiftRows, and AddRoundKey
+  int shiftedRow;
+  for (i = 0; i < 16; i++) {
+    shiftedRow = (5*(i%4) + 4*(i/4)) % 16;
+    //printf("%d ", shiftedRow);
+    sbox[i] = sub_bytes(s[shiftedRow]) ^ key[16*Nr+i];
+  }
+//  for (i = 0; i < 16; i++) {
+//    printf("%2x", sbox[i]);
+//    if ((i-3) % 4 == 0)
+//      printf("\n");
+//  }
+//  printf("\n");
+}
+
+
 byte *encrypt(byte *plaintext, int pt_length, byte *key, int mode) {
   // Select Mode and initialize mode-dependent values
   int Nk, Nr;
@@ -63,31 +134,35 @@ byte *encrypt(byte *plaintext, int pt_length, byte *key, int mode) {
   }
   Nr = Nk + 6;
   // Allocate state array
-  byte **state = calloc(STATEROWS, sizeof(byte *));
+  byte *state = calloc(STATEROWS * STATECOLS, sizeof(byte *));
   int i;
-  for (i = 0; i < STATEROWS; i++) {
-    state[i] = calloc(STATECOLS, sizeof(byte));
-  }
+//  for (i = 0; i < STATEROWS; i++) {
+//    state[i] = calloc(STATECOLS, sizeof(byte));
+//  }
   // Generate key schedule
   key_sched = malloc(Nb*(Nr+1) * sizeof(word));
   if (expand_key(key, key_sched, Nk)) {
     goto free_sched;
   }
-  for (int j = 0; j < 4*Nb*(Nr+1); j++) {
-    printf("%2x", key_sched[j]);
-    if ((j-3) % 4 == 0) {
+  // Process each block
+  for (int j = 0; j < 16; j++) {
+    state[j] = plaintext[j];
+  }
+  do_encrypt(state, key_sched, Nr);
+  for (i = 0; i < 16; i++) {
+    printf("%2x", state[i]);
+    if ((i-3) % 4 == 0)
       printf("\n");
-    }
   }
   printf("\n");
-  // Process each block
+
   
 free_sched:
   free(key_sched);
 free_state:
-  for (i = 0; i < STATEROWS; i++) {
-    free(state[i]);
-  }
+  //for (i = 0; i < STATEROWS; i++) {
+  //  free(state[i]);
+  //}
   free(state);
 exit:
   return NULL;
